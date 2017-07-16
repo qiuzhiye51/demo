a28e40d759bbaecb3d6fb7ffb855f6964c148fba
chenhao <731558954@qq.com>
2017-07-16 11:27:22 +0800

    测试增加内容

---
+++ demo/src/main/java/com/chenh/demo/User.java
@@ -0,0 +1,34 @@
+package com.chenh.demo;
+
+/**
+ * Created by Yoga710 on 2017/7/16.
+ */
+public class User {
+    private String name;
+    private int age;
+    private String sex;
+
+    public String getName() {
+        return name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public String getSex() {
+        return sex;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public void setSex(String sex) {
+        this.sex = sex;
+    }
+}
