d03769d4685717e24069bac89299f97aad24f9bc
chenhao <731558954@qq.com>
2017-07-16 11:26:50 +0800

    测试修改内容

--- demo/src/main/java/com/chenh/demo/Test.java
+++ demo/src/main/java/com/chenh/demo/Test.java
@@ -6,6 +6,11 @@ package com.chenh.demo;
 public class Test {
     public static void main(String[] args){
         System.out.println("hello world");
+        System.out.println("你好！");
+
+
+
+
 
     }
 
