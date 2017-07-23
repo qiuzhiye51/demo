30517714ab7bbb28b9c6fe86a0c875fb6a4a4b1f
qiuzhiye51 <731558954@qq.com>
2017-07-23 21:54:32 +0800

    Update Test.java
    gitHub修改Test.java 44444

--- demo/src/main/java/com/chenh/demo/Test.java
+++ demo/src/main/java/com/chenh/demo/Test.java
@@ -6,7 +6,7 @@ package com.chenh.demo;
 public class Test {
     public static void main(String[] args){
         System.out.println("hello world");
-        System.out.println("你好！22222");
+        System.out.println("你好！44444");
 
 
 
